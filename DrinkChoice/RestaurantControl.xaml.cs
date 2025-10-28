b90e4748621bc6322c77735846707382b1bb7c61
Hudson Boldt <hudsonboldtkc@gmail.com>
2025-10-27 21:46:46 -0500

    Programmatic Controls Tutorial

--- DrinkChoice/RestaurantControl.xaml.cs
+++ DrinkChoice/RestaurantControl.xaml.cs
@@ -23,6 +23,32 @@ namespace DrinkChoice
         public RestaurantControl()
         {
             InitializeComponent();
+            LoadChoices();
+        }
+
+    public void LoadChoices()
+        {
+            if (DataContext is Restaurant r)
+            {
+                StackPanel stack = new StackPanel();
+                foreach (SodaChoice choice in r.PossibleSodas)
+                {
+                    CheckBox box = new CheckBox();
+                    box.DataContext = choice;
+                    Binding binding = new Binding();
+                    binding.Path = new PropertyPath(nameof(choice.Chosen));
+                    binding.Mode = BindingMode.TwoWay;
+                    BindingOperations.SetBinding(box, CheckBox.IsCheckedProperty, binding);
+
+                    TextBlock block = new TextBlock();
+                    block.Text = choice.ToString();
+                    box.Content = block;
+
+
+                    stack.Children.Add(box);
+                }
+                restDock.Children.Add(stack);
+            }
         }
     }
 }
